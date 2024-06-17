2042b53687ecca54be69c5e210824066244128cd
erick-ondiwa <erickochieng3860@gmail.com>
2024-06-17 22:49:55 +0300

    Modified

--- scripts.js
+++ scripts.js
@@ -1,12 +1,26 @@
 function getTime(){
   const currentTime = new Date();
-  let hours = currentTime.getHours().toString().padStart(2, 0);
+  let hours = currentTime.getHours();
+  let meridium;
+  if(hours == 0 && minutes == 0){
+    meridium = "Midnight";
+  }
+  if(hours == 12 && minutes == 0){
+    meridium = "noon";
+  }
+  if(hours > 12){
+    meridium = "PM";
+  }
+  if(hours < 12){
+    meridium = "AM";
+  }
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2, 0);
   let minutes = currentTime.getMinutes().toString().padStart(2, 0);
-  let seconds = currentTime.getSeconds();
-  
+  let seconds = currentTime.getSeconds().toString().padStart(2, 0);
   let clockText = document.getElementById('clockText');
 
-  clockText.textContent = `${hours}:${minutes}:${seconds}`;
+  clockText.textContent = `${hours}:${minutes}:${seconds}${meridium}`;
 }
 
 setInterval(getTime, 1000);
